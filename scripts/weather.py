711a146fd5e3d200b4a5ead7f6566418d7a9a07a
Luke Mozena <lmozena75@gmail.com>
2020-06-13 16:30:15 -0500

    polybar added to dots

---
+++ scripts/weather.py
@@ -0,0 +1,36 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+# Procedure
+# Surf to https://openweathermap.org/city
+# Fill in your CITY
+# e.g. Antwerp Belgium
+# Check url
+# https://openweathermap.org/city/2803138
+# you will the city code at the end
+# create an account on this website
+# create an api key (free)
+# LANG included thanks to krive001 on discord
+
+
+import requests
+
+CITY = "4881346"
+API_KEY = "3f1532f8cc75e01530da9bfd73163827"
+UNITS = "Metric"
+UNIT_KEY = "C"
+LANG = "en"
+#LANG = "nl"
+#LANG = "hu"
+
+REQ = requests.get("http://api.openweathermap.org/data/2.5/weather?id={}&lang={}&appid={}&units={}".format(CITY, LANG,  API_KEY, UNITS))
+try:
+    # HTTP CODE = OK
+    if REQ.status_code == 200:
+        CURRENT = REQ.json()["weather"][0]["description"].capitalize()
+        TEMP = int(float(REQ.json()["main"]["temp"]))
+        print("{}, {} °{}".format(CURRENT, TEMP, UNIT_KEY))
+    else:
+        print("Luke-you no smart BAD HTTP STATUS CODE " + str(REQ.status_code))
+except (ValueError, IOError):
+    print("Error: Unable print the data")
